5b43414edf810be0bf8b1fd22e4f5a1cc607bb1f
annonymous <nghia.nvvt291@gmail.com>
2023-09-13 14:18:57 +0700

    update source

---
+++ database/migrations/2023_09_13_071647_create_sessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->string('payload')->nullable();
+            $table->string('last_activity')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->string('ip_address')->nullable();
+            $table->string('user_agent')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
